6062f9199b999ee546ca609b351de473fc7898fb
mohamedderbali <mohamed.derbali@esprit.tn>
2023-01-19 15:11:22 +0100

    update cli

--- fw.bat
+++ fw.bat
@@ -1,23 +1,36 @@
 @echo off
-echo "welcome to FUZZY WUZZY CLI" 
-@REM if(%1)==null
-@REM echo "asba"
-@REM if(%1)=="help"
-@REM echo "fw create"
-@REM echo "fw help"
-@REM echo "fw version"
-@REM echo "fw update"
-@REM echo "fw run"
-@REM echo "fw build"
-@REM echo "fw test"
-@REM echo "fw deploy"
-@REM echo "fw publish"
-@REM echo "fw install"
-@REM echo "fw uninstall"
-@REM echo "fw start"
-@REM echo "fw stop"
-@REM echo "fw restart"
-@REM echo "fw status"
-@REM echo "fw list"
-@REM if(%1)=="create"
-@REM git clone --single-branch --branch squeleton https://github.com/MohamedDerbali/FuzzyWuzzyJS.git
+SET ARG=%1
+SET SECOND_ARG=%2
+IF DEFINED ARG (
+    IF "%ARG%"=="--version" (
+        echo "Fuzzy Wuzzy CLI version 0.0.1"
+        goto end
+    )
+    IF "%ARG%"=="--help" (
+        echo "fw create"
+        echo "fw --help"
+        echo "fw version"
+        goto end
+    ) 
+    IF "%ARG%"=="create" (
+        IF DEFINED SECOND_ARG (
+            mkdir %SECOND_ARG% 
+            cd %SECOND_ARG%
+            git clone -b squeleton https://github.com/MohamedDerbali/FuzzyWuzzyJS.git
+            mv FuzzyWuzzyJS/* .
+            rmdir /s /q FuzzyWuzzyJS
+            cd ..
+            goto end 
+        )
+        IF "%SECOND_ARG%"=="" (
+            echo [31mError: Please specify a project name[0m
+            goto end
+        )
+    )
+)
+IF "%ARG%"=="" (
+    echo [31mError: Please specify an argument[0m
+    echo use --help to show an example
+    goto end
+)
+:end
